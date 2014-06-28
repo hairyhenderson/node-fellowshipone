f1105c88c2fc12efd4ebe7a93afe9bcb0fe3e0f1
Dave Henderson <dhenderson@gmail.com>
2014-06-28 11:21:49 -0400

    Got stuff working...

---
+++ test/f1_test.js
@@ -0,0 +1,52 @@
+'use strict'
+var sinon = require('sinon')
+var should = require('should')
+var request = require('request')
+var F1 = require('../lib/f1')
+
+describe('F1', function() {
+	var r
+	before(function() {
+		r = sinon.mock(request)
+	})
+	after(function() {
+		r.restore()
+	})
+	describe('constructor', function() {
+		it('errors given a non-object', function(done) {
+			should(function() {
+				new F1("not an object")
+			}).
+			throw ()
+			done()
+		})
+		it('errors given non-schema-valid credentials', function(done) {
+			should(function() {
+				new F1({
+					foo: true
+				})
+			}).
+			throw ()
+			done()
+		})
+		it('sets the OAuth credentials as default request options', function(done) {
+			var opts = {
+				consumer_key: 'key',
+				consumer_secret: 'secret',
+				token: 'token',
+				token_secret: 'secret'
+			}
+			var uri = 'http://example.com'
+			r.expects('get').withArgs({
+				uri: uri,
+				oauth: opts
+			}).yields()
+
+			var f1 = new F1(opts)
+			f1.request.get({ uri: uri }, function(err, res, body) {
+				r.verify()
+				done()
+			})
+		})
+	})
+})
---
+++ test/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+require('./f1object_test')
+require('./f1_test')
+require('./person_test')
