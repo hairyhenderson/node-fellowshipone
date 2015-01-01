da94198da512b5630f848c859e27af916a00275d
Dave Henderson <dhenderson@gmail.com>
2015-01-01 16:10:52 -0500

    Refactoring and adding People.show method

--- test/people_test.js
+++ test/people_test.js
@@ -6,7 +6,7 @@ var F1 = require('../lib/f1')
 var People = require('../lib/people')
 
 describe('People', function() {
-  var r, people, f1, config;
+  var r, people, f1, config, _people;
 
   beforeEach(function() {
     r = sinon.mock(request)
@@ -21,22 +21,26 @@ describe('People', function() {
     }
     f1 = new F1(config)
     people = new People(f1)
+    _people = sinon.mock(people)
   })
 
   function verifyAll() {
     r.verify()
+    _people.verify()
   }
 
   afterEach(function() {
     r.restore()
+    _people.restore()
   })
 
-  describe('list', function() {
+  describe('_get', function() {
     it('errors when request errors', function(done) {
       r.expects('get').yields('error')
 
-      people.list(0, function(err, result) {
+      people._get('/', function(err, result) {
         err.should.eql('error')
+        verifyAll()
         done()
       })
     })
@@ -47,23 +51,86 @@ describe('People', function() {
         headers: {}
       }, 'foo')
 
-      people.list(0, function(err, result) {
+      people._get('/', function(err, result) {
         err.should.eql({
           statusCode: 404,
           headers: {},
           message: 'foo'
         })
+        verifyAll()
+        done()
+      })
+    })
+
+    it('yields the body and headers when successful without query', function(done) {
+      r.expects('get').withArgs('http://example.com/mypath', {
+        oauth: {
+          consumer_key: '123',
+          consumer_secret: 'secret'
+        },
+        headers: {
+          accept: People.MEDIA_TYPE,
+          'content-type': People.MEDIA_TYPE
+        },
+        json: true
+      }).yields(null, {
+        statusCode: 200,
+        headers: {}
+      }, 'body')
+
+      people._get('/mypath', function(err, body, headers) {
+        body.should.eql('body')
+        headers.should.eql({})
+        verifyAll()
+        done()
+      })
+    })
+
+    it('yields the body and headers when successful with query', function(done) {
+      r.expects('get').withArgs('http://example.com/mypath', {
+        oauth: {
+          consumer_key: '123',
+          consumer_secret: 'secret'
+        },
+        headers: {
+          accept: People.MEDIA_TYPE,
+          'content-type': People.MEDIA_TYPE
+        },
+        json: true,
+        qs: {
+          query: 42
+        }
+      }).yields(null, {
+        statusCode: 200,
+        headers: {}
+      }, 'body')
+
+      people._get('/mypath', {
+        query: 42
+      }, function(err, body, headers) {
+        body.should.eql('body')
+        headers.should.eql({})
+        verifyAll()
+        done()
+      })
+    })
+  })
+
+  describe('list', function() {
+    it('errors when request errors', function(done) {
+      _people.expects('_get').yields('error')
+
+      people.list(0, function(err, result) {
+        err.should.eql('error')
+        verifyAll()
         done()
       })
     })
 
     it('errors when request yields unexpected object', function(done) {
-      r.expects('get').yields(null, {
-        statusCode: 200,
-        headers: {}
-      }, {
+      _people.expects('_get').yields(null, {
         foo: ''
-      })
+      }, {})
 
       people.list(42, function(err, results) {
         err.should.eql({
@@ -73,21 +140,67 @@ describe('People', function() {
             foo: ''
           }
         })
+        verifyAll()
         done()
       })
     })
 
     it('returns the list of people', function(done) {
-      r.expects('get').yields(null, {
-        statusCode: 200
-      }, {
+      _people.expects('_get').yields(null, {
         people: {
           person: [{}, {}]
         }
-      })
+      }, {})
 
       people.list(42, function(err, result) {
         result.should.eql([{}, {}])
+        verifyAll()
+        done()
+      })
+    })
+  })
+
+  describe('show', function() {
+    it('errors when request errors', function(done) {
+      _people.expects('_get').yields('error')
+
+      people.show(42, function(err, person) {
+        err.should.eql('error')
+        verifyAll()
+        done()
+      })
+    })
+
+    it('errors when request yields unexpected object', function(done) {
+      _people.expects('_get').withArgs('/Person/42').yields(null, {
+        foo: ''
+      })
+
+      people.show(42, function(err, person, headers) {
+        err.should.eql({
+          statusCode: 502,
+          headers: headers,
+          message: {
+            foo: ''
+          }
+        })
+        verifyAll()
+        done()
+      })
+    })
+
+    it('returns the person object', function(done) {
+      _people.expects('_get').withArgs('/Person/42').yields(null, {
+        person: {
+          firstName: 'Joe'
+        }
+      })
+
+      people.show(42, function(err, person) {
+        person.should.eql({
+          firstName: 'Joe'
+        })
+        verifyAll()
         done()
       })
     })
@@ -95,34 +208,17 @@ describe('People', function() {
 
   describe('search', function() {
     it('errors when request errors', function(done) {
-      r.expects('get').yields('error')
+      _people.expects('_get').yields('error')
 
       people.search({}, function(err, result) {
         err.should.eql('error')
-        done()
-      })
-    })
-
-    it('errors when request gives non-200 status', function(done) {
-      r.expects('get').yields(null, {
-        statusCode: 404,
-        headers: {}
-      }, 'foo')
-
-      people.search({}, function(err, result) {
-        err.should.eql({
-          statusCode: 404,
-          headers: {},
-          message: 'foo'
-        })
+        verifyAll()
         done()
       })
     })
 
     it('returns the search results', function(done) {
-      r.expects('get').yields(null, {
-        statusCode: 200
-      }, {
+      _people.expects('_get').yields(null, {
         results: 100
       })
 
@@ -130,25 +226,16 @@ describe('People', function() {
         result.should.eql({
           results: 100
         })
+        verifyAll()
         done()
       })
     })
 
     it('uses given search parameters', function(done) {
-      r.expects('get').withArgs(config.apiURL + '/People/Search', {
-        oauth: config.oauth_credentials,
-        headers: {
-          accept: 'application/vnd.fellowshiponeapi.com.people.people.v2+json',
-          'content-type': 'application/vnd.fellowshiponeapi.com.people.people.v2+json'
-        },
-        json: true,
-        qs: {
-          searchFor: 'foo',
-          include: 'addresses,communications,attributes,requirements'
-        }
+      _people.expects('_get').withArgs('/People/Search', {
+        searchFor: 'foo',
+        include: 'addresses,communications,attributes,requirements'
       }).yields(null, {
-        statusCode: 200
-      }, {
         results: 100
       })
 
@@ -158,6 +245,7 @@ describe('People', function() {
         result.should.eql({
           results: 100
         })
+        verifyAll()
         done()
       })
     })
