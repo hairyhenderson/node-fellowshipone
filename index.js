32a0393e037435e40b3a5eb8eaaed7e0ae780b1d
Dave Henderson <dhenderson@gmail.com>
2015-01-28 22:08:44 -0500

    Exporting new resources on F1 object. Whoops.

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 
 module.exports = require('./lib/f1')
 module.exports.People = require('./lib/people')
+module.exports.Households = require('./lib/households')
+module.exports.Statuses = require('./lib/statuses')
+module.exports.HouseholdMemberTypes = require('./lib/householdmembertypes')
