97989d946b23136750d6ede2294d769873baa772
Dave Henderson <dhenderson@gmail.com>
2016-01-30 17:50:06 -0500

    Adding Promise API compatibility
    Signed-off-by: Dave Henderson <dhenderson@gmail.com>

--- test/f1_test.js
+++ test/f1_test.js
@@ -1,4 +1,5 @@
 var sinon = require('sinon')
+require('sinon-as-promised')
 var should = require('should')
 var request = require('request')
 var F1 = require('../lib/f1')
@@ -176,6 +177,98 @@ describe('F1', function () {
       f1.authenticate(function (err) {
         arguments.length.should.eql(1)
         err.should.eql('err')
+        f.verify()
+        done()
+      })
+    })
+  })
+  // ########
+  describe('get_token (Promise version)', function () {
+    it('yields error from request', function (done) {
+      r.expects('post').yields('ERROR')
+
+      f1.get_token().catch(function (err) {
+        err.should.eql('ERROR')
+        verifyAll()
+        done()
+      })
+    })
+
+    it('yields error when non-OK response received', function (done) {
+      r.expects('post').yields(null, {
+        statusCode: 404
+      }, 'not found')
+
+      f1.get_token().catch(function (err) {
+        err.should.have.property('statusCode', 404)
+        verifyAll()
+        done()
+      })
+    })
+
+    it('yields updated credentials and user URL', function (done) {
+      r.expects('post').withArgs('http://example.com/PortalUser/AccessToken', {
+        oauth: config.oauth_credentials,
+        form: {
+          ec: f1._hash_creds('joe', 'swordfish')
+        }
+      }).yields(null, {
+        statusCode: 200,
+        headers: {
+          'content-location': 'http://example.com/user/1234',
+          oauth_token: '1234',
+          oauth_token_secret: 'toksecret'
+        }
+      }, '')
+
+      f1.get_token().then(function (creds) {
+        creds.should.have.property('token', '1234')
+        creds.should.have.property('token_secret', 'toksecret')
+        // url.should.eql('http://example.com/user/1234')
+        verifyAll()
+        done()
+      })
+    })
+
+    it('adds token, secret, and URL to original config object', function (done) {
+      r.expects('post').withArgs('http://example.com/PortalUser/AccessToken', {
+        oauth: config.oauth_credentials,
+        form: {
+          ec: f1._hash_creds('joe', 'swordfish')
+        }
+      }).yields(null, {
+        statusCode: 200,
+        headers: {
+          'content-location': 'http://example.com/user/1234',
+          oauth_token: '1234',
+          oauth_token_secret: 'toksecret'
+        }
+      }, '')
+
+      f1.get_token().then(function (creds) {
+        config.oauth_credentials.should.have.property('token', '1234')
+        config.oauth_credentials.should.have.property('token_secret', 'toksecret')
+        config.userURL.should.eql('http://example.com/user/1234')
+        verifyAll()
+        done()
+      })
+    })
+  })
+
+  describe('authenticate', function () {
+    var f
+    beforeEach(function () {
+      f = sinon.mock(f1)
+    })
+    afterEach(function () {
+      f.restore()
+    })
+    it('delegates to get_token', function (done) {
+      f.expects('get_token').resolves('creds')
+
+      f1.authenticate().then(function (result) {
+        arguments.length.should.eql(1)
+        result.should.eql('creds')
         f.verify()
         done()
       })
